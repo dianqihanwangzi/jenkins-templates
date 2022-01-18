0e257c427c9941a644bdd697fd2b04851498e054
dianqihanwangzi <dianqihanwangzi@gmail.com>
2022-01-18 14:54:45 +0800

    add parse build result function

--- tipipeline/checkers/jenkins-it-trigger-check.groovy
+++ tipipeline/checkers/jenkins-it-trigger-check.groovy
@@ -8,6 +8,36 @@ properties([
         ])
 ])
 
+@NonCPS // has to be NonCPS or the build breaks on the call to .each
+def parseBuildResult(list) {
+    def total_test = 0
+    def failed_test = 0
+    def success_test = 0
+
+    list.each { item ->
+        echo "${item}"
+        if (item.status == "success") {
+            success_test += 1
+        } else {
+            failed_test += 1
+        }
+    }
+    total_test = success_test + failed_test
+
+    println "total_test: ${total_test}"
+    println "success_test: ${success_test}"
+    println "failed_test: ${failed_test}"
+
+    def resp_str = ""
+    if (failed_test > 0) {
+        resp_str = "failed ${failed_test}, success ${success_test}, total ${total_test}"
+    } else {
+        resp_str = "all ${total_test} tests passed"
+    }
+
+    return resp_str      
+}
+
 common = {}
 node("lightweight_pod") {
     container("golang") {
