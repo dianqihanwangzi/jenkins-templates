04d6802ca7b83d6d7667968a9d077ad1baa2ced3
dianqihanwangzi <dianqihanwangzi@gmail.com>
2022-01-24 17:43:26 +0800

    update merge ci commit id status (#123)
    * update status to tidb
    
    * fix synax error
    
    * set pipeline properties
    
    * add pipeline params
    
    * set jenkins url properties
    
    * set pipeline run id
    
    * fail pipeline if task failed
    
    * fix jenkins loop error
    
    * rename function
    
    * fix length
    
    * fix length fuction
    
    * fix url not found
    
    * fix spell
    
    * update task status
    
    * fix synax error
    
    * throw exception when failed
    
    * use light weight pod instead
    
    * rename checker
    
    * inject env vars
    
    * support other checkers
    
    * rename jenkins it trigger
    
    * fix spell error
    
    * add parse build result function
    
    * update merge ci commit id status
    
    * remove debug code

--- tipipeline/pipeline-templates/merge-check.groovy
+++ tipipeline/pipeline-templates/merge-check.groovy
@@ -8,6 +8,7 @@
 //         ])
 // ])
 
+
 BRANCH = REF
 if (REF.startsWith("refs/heads/")) {
     BRANCH = REF.replaceAll("refs/heads/","")
@@ -30,5 +31,54 @@ node("lightweight_pod") {
 
         pipelineSpec = common.loadPipelineConfig(PIPELINE_YAML, PULL_REQUEST_AUTHOR, "")
         common.runPipeline(pipelineSpec, "merge", BRANCH, COMMIT_ID, "", "")
+        updateCommitIDStatus(pipelineSpec)
+    }
+}
+
+def updateCommitIDStatus(pipeline) {
+    node("github-status-updater") {
+        stage("Update commit status") {
+            container("github-status-updater") {
+                if (currentBuild.result == "ABORTED") {
+                    DESCRIPTION = 'Jenkins job aborted'
+                    // Commit state. Possible values are 'pending', 'success', 'error' or 'failure'
+                    STATUS = 'error'
+                } else if (currentBuild.result == "FAILURE") {
+                    DESCRIPTION = 'Jenkins job failed'
+                    STATUS = 'failure'
+                } else if (currentBuild.result == "SUCCESS") {
+                    DESCRIPTION = 'Jenkins job success'
+                    STATUS = 'success'
+                } else {
+                    DESCRIPTION = 'Jenkins job meets something wrong'
+                    STATUS = 'error'
+                }
+                sh """
+            cat > env_param.conf <<EOF
+export org=${pipeline.owner}
+export repo=${pipeline.repo}
+export COMMIT_ID="${pipeline.commitID}"
+export STATUS="${STATUS}"
+export DESCRIPTION="${DESCRIPTION}"
+EOF
+                """
+                withCredentials([string(credentialsId: 'sre-bot-token', variable: 'TOKEN')]) {
+                    sh """#!/bin/bash
+                    set +x
+                    source env_param.conf
+                    github-status-updater \
+                        -action update_state \
+                        -token ${TOKEN} \
+                        -owner ${pipeline.owner} \
+                        -repo ${pipeline.repo} \
+                        -ref  ${pipeline.commitID} \
+                        -state ${STATUS} \
+                        -context ${pipeline.repo}/merge-ci \
+                        -description "${DESCRIPTION}" \
+                        -url "${RUN_DISPLAY_URL}"
+                    """
+                }
+            }
+        }
     }
 }
